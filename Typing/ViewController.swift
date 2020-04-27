07ad77cca6f573a0e5fb427e6a18a658b403dfda
Keigo Saito <saito991203@ezweb.ne.jp>
2020-04-27 13:17:54 +0900

    Initial Commit

---
+++ Typing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Typing
+//
+//  Created by 斉藤啓悟 on 2020/04/27.
+//  Copyright © 2020 斉藤啓悟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
