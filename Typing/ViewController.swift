046ae9bb324ac02d1f43bbb3bdaeed0540a619c1
Keigo Saito <saito991203@ezweb.ne.jp>
2020-04-30 19:56:36 +0900

    aaa

--- Typing/ViewController.swift
+++ Typing/ViewController.swift
@@ -10,11 +10,327 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var field: UITextField!
+    @IBOutlet weak var good: UILabel!
+    @IBOutlet weak var coun: UILabel!
+    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var ans: UILabel!
+    @IBOutlet weak var backHome: UIButton!
+    @IBOutlet weak var retry: UIButton!
+    
+    
+    
+    
+    
+    
+    var count = 0
+    var abc = true
+    var quesCount = 0
+    var myTimer: Timer!
+    var usedTime1 :Double = 0
+    var letterCount = 0
+    var record:[Double] = [0,0,0]
+    var lpm:Double = 0
+    var choice:Int = 0
+    var stop = false
+    var started = false
+    var finish = false
+    let userDefaults = UserDefaults.standard
+     let kind = ["word","sentence","English"]
+    
+
+     var timerr: [Int] = [10,0,0]
+    @IBOutlet weak var time: UILabel!
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        self.view.backgroundColor = UIColor.cyan
+        
+        Random()
+        aaa()
+        count = 0
+        retry.setTitle("Retry", for: .normal)
+        
+        time.text = String(timerr[0])+":"+String(timerr[1])+String(timerr[2])
+        
+      
+        
+        userDefaults.register(defaults: ["word": [0,0,0]])
+               
+        userDefaults.register(defaults: ["sentence": [0,0,0]])
+               
+        userDefaults.register(defaults: ["English": [0,0,0]])
+        
+          record = userDefaults.object(forKey: kind[choice]) as! [Double]
+               
+        
     }
+    
+    @objc func timer(){
+        if (timerr[0] == 0 && timerr[1] == 0 && timerr[2] == 0) {
+            finish = true
+            performSegue(withIdentifier: "toSecond", sender: nil)
+            
+        } else {
+            if timerr[2] > 0 {
+                //秒数が0以上の時秒数を-1
+                timerr[2] -= 1
+            } else {
+                //秒数が0の時
+                timerr[2] += 9
+                if timerr[1] > 0 {
+                    //分が0以上の時、分を-1
+                    timerr[1] -= 1
+                } else {
+                    //分が０の時、+59分、時間-1
+                    timerr[1] += 9
+                    timerr[0] -= 1
+                }
+            }
+            time.text = String(timerr[0]) + ":" + String(timerr[1])+String(timerr[2])
+        }
+    }
+   
 
+    @IBAction func EditingDidBegin(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector:#selector(timer) , userInfo: nil, repeats: true)
+        started = true
+        stop = true
+        retry.setTitle("Stop", for: .normal)
+    }
+    
 
+    @IBAction func EditingChanged(_ sender: Any) {
+        judge()
+        
+    }
+    
+    func aaa(){
+        coun.text = String(count)
+    }
+    
+    @IBAction func retryyy(_ sender: Any) {
+        
+        
+        
+        if started == false{
+            count = 0
+            abc = true
+            quesCount = 0
+            usedTime1 = 0
+            letterCount = 0
+            timerr = [10,0,0]
+            stop = false
+            
+            
+            self.loadView()
+            self.viewDidLoad()
+        }
+        if started == true && stop == true{
+            myTimer.invalidate()
+            stop = false
+            retry.setTitle("Retry", for: .normal)
+        }else if started == true && stop == false{
+            count = 0
+            abc = true
+            quesCount = 0
+            usedTime1 = 0
+            letterCount = 0
+            timerr = [10,0,0]
+            stop = false
+            
+            
+            self.loadView()
+            self.viewDidLoad()
+        }
+        
+
+    }
+    
+   
+    
+    func judge(){
+       
+        
+    if quesCount<1{
+        if field.text! == question.text!{
+            Random()
+            abc = true
+            field.text = ""
+            quesCount += 1
+            letterCount += question.text!.count
+        }else if field.text! == question.text!.prefix(field.text!.count){
+            good.text = "Good!"
+            abc = true
+           ans.text = String(question.text!.suffix(question.text!.count-field.text!.count))
+            
+            
+            
+           }else if abc == true{
+               good.text = "Bad!"
+               count += 1
+                aaa()
+                abc = false
+           }else{
+                good.text = "Bad!"
+                abc = false
+           }
+        }else{
+            if field.text! == question.text!{
+                
+               
+                field.text = ""
+                ans.text = ""
+                quesCount = 0
+                letterCount += question.text!.count
+                myTimer.invalidate()
+                
+                
+                usedTime1 = 10 - Double(timerr[0]) - Double(timerr[1])/10 - Double(timerr[2])/100
+                
+              
+                
+                 let lpm = round(60*Double(letterCount)/usedTime1)
+                
+                
+                if lpm >= record[0]{
+                    record[2] = record[1]
+                    record[1] = record[0]
+                    record[0] = lpm
+                }else if lpm >= record[1]{
+                    record[2] = record[1]
+                    record[1] = lpm
+                }else if lpm >= record[2]{
+                    record[2] = lpm
+                }
+                
+                SaveData(hairetsu:record,sort:kind[choice])
+
+                good.text = String(record[0])+","+String(record[1])+","+String(record[2])
+                
+                 performSegue(withIdentifier: "toSecond", sender: nil)
+                
+            }else if field.text! == question.text!.prefix(field.text!.count){
+             good.text = "Good!"
+             abc = true
+            ans.text = String(question.text!.suffix(question.text!.count-field.text!.count))
+            
+             
+             
+            }else if abc == true{
+                good.text = "Bad!"
+                count += 1
+                 aaa()
+                 abc = false
+            }else{
+                 good.text = "Bad!"
+                 abc = false
+            }
+        }//else
+        
+    }//judge
+    
+    func SaveData(hairetsu:[Double],sort:String){
+           userDefaults.set(hairetsu, forKey: sort)
+       }
+    
+    
+    
+    @IBAction func back(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+        
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+                if segue.identifier == "toSecond"{
+                    let second = segue.destination as! SecondViewController
+                    second.misss = String(count)
+                    second.usedTimee  = usedTime1
+                    second.rrrecorddd =  record
+                    second.letterCount2 = Double(letterCount)
+                    second.time = finish
+                   
+                
+    }
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+       func Random(){
+           var randomNumber = arc4random() % 4
+           randomNumber += 1
+        if choice == 0{
+           switch (randomNumber) {
+           case 1:
+               question.text = "ああ"
+               
+               break
+           case 2:
+               question.text = "いい"
+               break
+           case 3:
+               question.text = "うう"
+               break
+           default:
+               question.text = "ええ"
+               break
+           }
+        }
+        
+        
+        if choice == 1{
+           switch (randomNumber) {
+           case 1:
+               question.text = "あいうえお"
+               
+               break
+           case 2:
+               question.text = "かきくけこ"
+               break
+           case 3:
+               question.text = "さしすせそ"
+               break
+           default:
+               question.text = "もはじすあ"
+               break
+           }
+        }
+        
+        if choice == 2{
+           switch (randomNumber) {
+           case 1:
+               question.text = "word"
+               
+               break
+           case 2:
+               question.text = "take"
+               break
+           case 3:
+               question.text = "and"
+               break
+           default:
+               question.text = "look"
+               break
+           }
+        }
+        ans.text = question.text!
+           
+           
+       }
+ 
+    
 }
 
